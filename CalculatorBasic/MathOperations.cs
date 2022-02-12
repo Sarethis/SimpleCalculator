907569cde954623379137c02784f8a6c5e97b435
Sarethis <mateusz.kiela@gmail.com>
2022-02-12 20:12:19 +0100

    Add project files.

---
+++ CalculatorBasic/MathOperations.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorBasic
+{
+    internal static class MathOperations
+    {
+        static private decimal a, b, c, d;
+
+        
+        public static decimal Add (decimal a, decimal b)
+        {
+            return a + b;
+        }
+        public static decimal Subtract (decimal a, decimal b)
+        {
+            return a - b;
+        }
+        public static decimal Multiplication (decimal a, decimal b)
+        {
+            return (a * b);
+        }
+        public static decimal Division (decimal a, decimal b)
+        {
+            return (a / b);
+        }
+
+        public static decimal Calculate(decimal a, decimal b, int mathChoice)
+        {
+            switch (mathChoice)
+            {
+                case 1:
+                    return a + b;
+                case 2:
+                    return a - b;
+                case 3:
+                    return (a * b);
+                case 4:
+                    return (a / b);
+                case 5:
+                    {
+                        Environment.Exit(0);
+                        return 1;
+                    }
+                default:
+                    throw new Exception("Wybrałeś złą opcję");
+            }
+        }
+
+    }
+}
---
+++ CalculatorBasic/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorBasic
+{
+    internal class Program
+    {
+        static public decimal GetInput()
+        {
+            if (!decimal.TryParse(Console.ReadLine(), out decimal variable))
+            {
+                throw new Exception("Podana wartość nie jest liczbą.");
+            }
+            return variable;
+    }
+
+    static void Main(string[] args)
+        {
+            int mathChoice;
+            decimal varA, varB;
+
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Cześć, to jest prosty kalkulator!\nZaznacz jaką chcesz wykonać operację:");
+                    Console.WriteLine("1. Dodawanie");
+                    Console.WriteLine("2. Odejmowanie");
+                    Console.WriteLine("3. Mnożenie");
+                    Console.WriteLine("4. Dzielenie");
+                    Console.WriteLine("5. Wyjście");
+                    Console.Write("Twój wybór: ");
+
+                    if (!int.TryParse(Console.ReadLine(), out mathChoice))
+                    {
+                        throw new Exception("Podana wartość nie jest liczbą z zakresu 1 - 5.");
+                    }
+
+                    if (mathChoice == 5)
+                    {
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.Write("Podaj pierwszą wartość do operacji matematycznej: ");
+                        varA = GetInput();
+
+                        Console.Write("Podaj drugą wartość do operacji matematycznej: ");
+                        varB = GetInput();
+
+                        Console.WriteLine($"Wynik działania to = {MathOperations.Calculate(varA, varB, mathChoice)}");
+                    }
+
+                //    if (mathChoice == 5)
+                //    {
+                //        Environment.Exit(0);
+                //    }
+                //    else if (mathChoice == 1 || mathChoice == 2 || mathChoice == 3 || mathChoice == 4)
+                //    {
+                //        Console.Write("Podaj pierwszą wartość do operacji matematycznej: ");
+                //        varA = GetInput();
+
+                //        Console.Write("Podaj drugą wartość do operacji matematycznej: ");
+                //        varB = GetInput();
+
+
+
+                //        switch (mathChoice)
+                //        {
+                //            case 1:
+                //                {
+                //                    Console.WriteLine("\nWynik to= " + MathOperations.Add(varA, varB));
+                //                    break;
+                //                }
+                //            case 2:
+                //                {
+                //                    Console.WriteLine("\nWynik to= " + MathOperations.Subtract(varA, varB));
+                //                    break;
+                //                }
+                //            case 3:
+                //                {
+                //                    Console.WriteLine("\nWynik to= " + MathOperations.Multiplication(varA, varB));
+                //                    break;
+                //                }
+                //            case 4:
+                //                {
+                //                    Console.WriteLine("\nWynik to= " + MathOperations.Division(varA, varB));
+                //                    break;
+                //                }
+                //            default: break;
+                //        }
+                //    }
+                //    else if (mathChoice != 1 || mathChoice != 2 || mathChoice != 3 || mathChoice != 4)
+                //    {
+                //        throw new Exception("Wybrałeś złą operację.");
+                //    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+                finally
+                {
+                    Console.WriteLine();
+                }
+            }           
+        }
+    }
+}
